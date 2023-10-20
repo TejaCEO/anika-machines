32620d4b299508da8f312ee62d10b8021b594d27
Tejakavali <tejakavali@debian-BULLSEYE-live-builder-AMD64>
2023-10-20 22:59:26 +0530

    First Commit

---
+++ js/client-carousel.js
@@ -0,0 +1,11 @@
+//client
+$(document).on('ready', function() {
+	"use strict"
+  $("#client").owlCarousel({
+    items : 3,
+    lazyLoad : true,
+    navigation : true,
+  autoPlay:3000
+  }); 
+ 
+});
---
+++ js/history-carousel.js
@@ -0,0 +1,11 @@
+//about history
+$(document).on('ready', function() {
+ 	"use strict"
+  $("#about-history").owlCarousel({
+    items : 4,
+    lazyLoad : true,
+    navigation : true,
+  	autoPlay:3000
+  }); 
+ 
+});
